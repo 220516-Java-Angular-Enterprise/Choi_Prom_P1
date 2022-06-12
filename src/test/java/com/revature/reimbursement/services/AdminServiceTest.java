8fc61bcae4c170b5f0fda385709e9cdfe94f387d
Joseph Choi <joseph375@revature.net>
2022-06-12 17:45:34 -0400

    Added skeleton for test cases.

---
+++ src/test/java/com/revature/reimbursement/services/AdminServiceTest.java
@@ -0,0 +1,20 @@
+package com.revature.reimbursement.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AdminServiceTest {
+
+    @Test
+    public void setUserActivity() {
+    }
+
+    @Test
+    public void setUserRole() {
+    }
+
+    @Test
+    public void changeUserPassword() {
+    }
+}
